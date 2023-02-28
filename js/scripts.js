b312d96592dbf1a2c59d275118b506c28b59acb8
nav-leelarathna <navindul@icloud.com>
2023-02-28 17:04:44 +0000

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwLGQCdoZEX1P4Pvdnn6Xn_YyU3nauLKH4BWMc4xo7WKjbYv7cyz3OnMV_nLhmsg8ObWg/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
